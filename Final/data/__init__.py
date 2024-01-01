a2e8c3b39483ae7106280e39a89a56c73d9b2163
Shu Liu <liushu_public@yeah.net>
2024-01-01 23:15:19 +0800

    update data

---
+++ Final/data/__init__.py
@@ -0,0 +1,6 @@
+REFER = 'https://hifld-geoplatform.opendata.arcgis.com/search?collection=Dataset&groupIds=2900322cc0b14948a74dca886b7d7cfc'
+
+source_dir = 'D:/DataHub/Homeland Infrastructure Foundation-Level Data/'
+CITY = 'SAN FRANCISCO'
+
+
---
+++ Final/data/configs.py
@@ -0,0 +1,6 @@
+params1 = {
+	"num_iter": 10000,
+	"num_particle": 1000,
+	"num_station": 5,
+	"distance_limit": 30,  # km
+}
