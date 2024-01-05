c2cb776a005cff1839af39a2d2a200143da520dd
Shu Liu <liushu_public@yeah.net>
2024-01-05 18:59:26 +0800

    add batch configs

--- Final/run.py
+++ Final/run.py
@@ -1,17 +1,25 @@
 import os
+import sys
+
+if sys.platform == 'win32':
+    raise RuntimeError("No Windows! Linux Please.")
+
+import numpy as np
 import pandas as pd
 import data
 from data import configs
 from model import flp
 from datetime import datetime
-from multiprocessing import Queue, Process
+from multiprocessing import Process, Manager
+from concurrent.futures import ProcessPoolExecutor, Future
 
 
 class Logger(Process):
     def __init__(self, home: str = './log/'):
         super().__init__()
 
-        self.queue = Queue()
+        self.manager = Manager()
+        self.queue = self.manager.Queue()
         self.daemon = True
         self.home = home + datetime.now().strftime("%Y-%m-%d-%H-%M-%S/")
 
@@ -21,28 +29,56 @@ class Logger(Process):
 
     def run(self):
         print("Logger start")
-        with open(self.home + 'log.csv', mode='a', encoding='utf-8') as f:
-            f.write('num_iter,num_particle,duration(s),space_violation,demand_violation,fitness,config,best_coord\n')
 
+        # header: num_iter,duration(s),num_illegal,space_violation,demand_violation,fitness,config
         while True:
-            i, n, duration, space_violation, demand_violation, fitness, best_pos, *config = self.queue.get()
-            print(f"""[iter {i} particle {n} ({duration: .3f}s) (config: {config})] space violation: {space_violation}, demand violation: {demand_violation}, fitness: {fitness}""")
-            with open(self.home + 'log.csv', mode='a', encoding='utf-8') as f:
-                f.write(f'{i},{n},{duration},{space_violation},{demand_violation},{fitness},{config}\n')
-            with open(self.home + 'best_pos.csv', mode='a', encoding='utf-8') as f:
-                f.write(str(best_pos.flatten().tolist()).lstrip('[').rstrip(']') + '\n')
+            label, num_illegal, space_violation, demand_violation, fitness, pos, *config = self.queue.get()
+            if label[0] == 'global best':
+                i = label[1]
+                duration = ""
+                log_name, pos_name = 'best_log', 'best_pos'
+                # print(f"""[iter {i} (config: {config})] num illegal: {num_illegal} space violation: {space_violation}, demand violation: {demand_violation}, fitness: {fitness}""")
+            elif label[0] == 'track':
+                n, i, duration = label[1:]
+                log_name, pos_name = f'track {n} log', f'track {n} pos'
+            else:
+                raise ValueError(f"Undefined label: {label}")
+
+            with open(self.home + f'{log_name}.csv', mode='a', encoding='utf-8') as f:
+                f.write(f'{i},{duration},{num_illegal},{space_violation},{demand_violation},{fitness},{config}\n')
+            with open(self.home + f'{pos_name}.csv', mode='a', encoding='utf-8') as f:
+                f.write(str(pos.flatten().tolist()).lstrip('[').rstrip(']') + '\n')
+
+
+def test_batch(demand_coords: np.ndarray, start_coord: np.ndarray, name: str, configs: list[dict]):
+    pool = ProcessPoolExecutor()
+    print(f"Start test_{name}")
+
+    futures: list[Future] = [pool.submit(
+        flp.pso,
+        demand_coords, start_coord, logger.queue, **config, track=list(range(5))
+    ) for config in configs]
+    [future.result() for future in futures]
+
+    print(f"Finish test_{name}")
+    pool.shutdown()
 
 
 if __name__ == '__main__':
     logger = Logger()
     logger.start()
 
-    table = pd.read_csv(data.source_dir + data.CITY + '.csv')
+    table = pd.read_csv(data.source_dir_wsl + data.CITY + '.csv')
     start_point = table[table['TYPE'] == 'Public_Health_Departments']
 
     start_coord = start_point.loc[:, ['X', 'Y']].to_numpy()
     demand_coords = table.loc[:, ['X', 'Y']].to_numpy()
 
-    flp.pso(demand_coords, start_coord, logger.queue, **configs.params1)
+    # flp.pso(demand_coords, start_coord, logger.queue, **configs['params'], track=list(range(10)))
+    # test_batch(demand_coords, start_coord, 'omega', data.batch_for_omega)
+    # test_batch(demand_coords, start_coord, 'distance_limit', data.batch_for_distance_limit)
+    test_batch(demand_coords, start_coord, 'num_station', data.batch_for_num_station)
+
+    logger.queue.join()
     # TODO: test the code without parallel
     # TODO: comparison among different params in parallel
